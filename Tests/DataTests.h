341651bbe0963207ae47bf24dd43d04eed77cdf0
godexsoft <godexsoft@cebd784a-b4ba-d164-4192-445bf20e9c98>
2011-03-23 17:27:31 +0000

    super primitive tests for Data

--- Tests/DataTests.h
+++ Tests/DataTests.h
@@ -12,7 +12,20 @@ SUITE(Data);
 // Data simple tests
 ////////////////////////////////////////////////////////////////////
 TEST(Simple, Data) {
-    // TODO: write some tests
+    Data d0;
+    rassert(d0.empty()==true);
+    
+    Data d1(10);
+    rassert(d1.empty()==false);
+    
+    d1.fill(0);
+    rassert(d1.get_data()[0] == 0);
+    d1.fill(1);
+    rassert(d1.get_data()[0] == 1);
+    
+    rassert(d0 != d1);
+    d0 = d1;
+    rassert(d0 == d1);
 };
 
 #endif // __MEMORY_TESTS_H__
